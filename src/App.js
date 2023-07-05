3acec90f9e5549adeb9eebbf9151a641dd4a59ee
Leo Zhang <t-leozhang@microsoft.com>
2023-07-05 11:29:11 -0700

    testing status

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState('');
+  const [data, setData] = useState('Loading msg from API...');
 
   useEffect(() => {
     (async function () {
       const API_PATH = '/api/orchestrators/HelloOrchestrator'
 
-      const content = await( await fetch(API_PATH)).json();
-      console.log(content);
-      setData(content);
+      const { statusQueryGetUri } = await( await fetch(API_PATH)).json();
+      const { status } = await (await fetch(statusQueryGetUri)).json();
+      console.log(statusQueryGetUri);
+      console.log(status)
+      setData(statusQueryGetUri);
     })();
   });
 
-  return <div>Hello {data}</div>;
+  return <div>{data}</div>;
 }
 
 export default App;
