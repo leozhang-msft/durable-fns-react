273830e61cbfc0946b232886ea78d939c2203e6c
Leo Zhang <t-leozhang@microsoft.com>
2023-07-05 09:57:33 -0700

    test linked durable fn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const API_PATH = '/api/orchestrators/HelloOrchestrator'
+
+      const content = await( await fetch(API_PATH)).json();
+      console.log(content);
+      // setData(text);
+    })();
+  });
+
+  return <div>Hello</div>;
 }
 
 export default App;
